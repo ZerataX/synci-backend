11ab6db196611e3efe35add1a36f313f1e557c5c
ZerataX <mail@zera.tax>
2020-10-20 18:59:37 +0200

    json to message overload

--- src/websocket.cr
+++ src/websocket.cr
@@ -13,8 +13,19 @@ end
 
 class Message
   getter data : String
+  getter type : String
+
+  def initialize(json : JSON::Any)
+    if json["type"]?
+      @data = json.to_json.not_nil!
+      @type = json["type"].as_s
+    else
+      raise KeyError.new %(missing key "type")
+    end
+  end
 
   def initialize(type, **values : String | Bool | Number)
+    @type = type
     @data = JSON.build do |json|
       json.object do
         json.field "type", type
@@ -32,7 +43,6 @@ end
 
 def sendToAll(users : Enumerable, author_id : UUID, message : Message)
   users.each do |user|
-    puts "#{user.id} compare with #{author_id}"
     unless user.id == author_id
       user.socket.send message.data
     end
@@ -53,21 +63,20 @@ ws "/session/:name" do |socket, context|
   uuid = UUID.random
 
   # Send welcome message to the client
-  sendTo(socket, Message.new type: "connect", message: "Hello #{name}")
+  sendTo(socket, Message.new type: "connect", message: "Welcome to #{name}!")
 
   # Handle incoming message
-  socket.on_message do |message|
-    json = JSON.parse(message)
-    type = json["type"]
+  socket.on_message do |data|
+    message = Message.new(JSON.parse(data).not_nil!) # TODO: exception handling
 
-    case type
+    case message.type
     when "login"
       
       session.add(User.new(socket, uuid))
       sendToAll(session, uuid, Message.new type: "updateUsers", id: uuid.to_json)
       sendTo(socket, Message.new type: "login", success: true)
     else
-      sendTo(socket, Message.new type: "error", message: "Command not found: #{type}")
+      sendTo(socket, Message.new type: "error", message: "Command not found: #{message.type}")
     end
   end
 
